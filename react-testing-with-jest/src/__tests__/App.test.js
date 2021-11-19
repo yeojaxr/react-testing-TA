1ab3c234ce96ea0eee2635f270e17a2437c550f9
yeojaxr <ikenurulfatihah23@gmail.com>
2021-11-19 15:29:59 +0700

    done

--- react-testing-with-jest/src/__tests__/App.test.js
+++ react-testing-with-jest/src/__tests__/App.test.js
@@ -2,17 +2,24 @@ import { AddTodo } from '../components/helper';
 
 test('Should add todo to the list', () => {
   // Arrange
+  const list = [1, 2]
+  const newItem = 3
 
+  const expectedResult = [newItem, ...list]
   // Act
-
+  const newList = AddTodo(list, newItem)
   // Assert
+  expect(newList).toStrictEqual(expectedResult)
 })
 
 test('should not mutate the existing todo array', () => {
   // Arrange
+  const list = [1, 2]
+  const newItem = 3
+  const wrongResult = [newItem]
 
   // Act
-
+  const newList = AddTodo(list, newItem)
   // Assert
-
+  expect(newList).not.toBe(wrongResult)
 })
--- react-testing-with-jest/src/__tests__/Math.test.js
+++ react-testing-with-jest/src/__tests__/Math.test.js
@@ -2,32 +2,39 @@ import { Add, Substract, Multiplication } from '../Math'
 
 test('Return sum of two arguments', () => {
   // Arrange
+  const a = 4
+  const b = 5
+  const expectedResult = 9
 
   // Act
-
+  const result = Add(a, b)
 
   // Assert
-
+  expect(result).toBe(expectedResult)
 })
 
 test('Return substraction of a - b', () => {
   // Arrange
-
+  const a = 8
+  const b = 3
+  const expectedResult = 5
 
   // Act
-
+  const result = Substract(a, b)
 
   // Assert
-
+  expect(result).toBe(expectedResult)
 })
 
 test('Return multiplication of two arguments', () => {
   // Arrange
-
+  const a = 3
+  const b = 4
+  const expectedResult = 12
 
   // Act
-
+  const result = Multiplication(a, b)
 
   // Assert
-
+  expect(result).toBe(expectedResult)
 })
