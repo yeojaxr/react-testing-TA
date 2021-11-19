eb1fe63828b512b3b789957b9ed85d22ba96e792
yeojaxr <ikenurulfatihah23@gmail.com>
2021-11-19 15:09:06 +0700

    template

---
+++ react-testing-with-jest/src/__tests__/App.test.js
@@ -0,0 +1,18 @@
+import { AddTodo } from '../components/helper';
+
+test('Should add todo to the list', () => {
+  // Arrange
+
+  // Act
+
+  // Assert
+})
+
+test('should not mutate the existing todo array', () => {
+  // Arrange
+
+  // Act
+
+  // Assert
+
+})
---
+++ react-testing-with-jest/src/__tests__/Math.test.js
@@ -0,0 +1,33 @@
+import { Add, Substract, Multiplication } from '../Math'
+
+test('Return sum of two arguments', () => {
+  // Arrange
+
+  // Act
+
+
+  // Assert
+
+})
+
+test('Return substraction of a - b', () => {
+  // Arrange
+
+
+  // Act
+
+
+  // Assert
+
+})
+
+test('Return multiplication of two arguments', () => {
+  // Arrange
+
+
+  // Act
+
+
+  // Assert
+
+})
